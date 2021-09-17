a342258d75982b515d8c3577df2cd8defe573e50
Christine Rowland <fordo29@gmail.com>
2021-09-16 20:35:36 -0600

    Add variable practice

---
+++ model-1.js
@@ -0,0 +1,21 @@
+//Practicing variables
+//Example big picture raising kids
+
+// String variables
+var needOne = "House"
+var needTwo = "Car"
+var needThree = "Money"
+
+// Number variables
+var mortage = 1600
+var numKids = 3
+
+//boolean variables
+var isHungry = true
+var isSleepy = false
+var isBehaving = false
+
+//array variables
+var kidNames = ["Olivia", "Ethan", "Caroline"]
+var kidHobbies = ["eating", "playing on device", "swimming", "dancing"]
+var kidAge = [8, 8, 6]
